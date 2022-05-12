eb654e33ef30f702225cec8b6948f6d4fb6ff93e
Steven Hoyos <stevendavidhoyosvasquez@gmail.com>
2022-05-11 19:57:20 -0500

    CoffeBreak 1.0 Version.

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/* Open and close menu hamburguer */
+
+const toggleButton = document.getElementById("button-menu");
+const navWrapper = document.getElementById("nav");
+
+
+toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("close");
+  navWrapper.classList.toggle("show");
+});
+ 
+/* Close menu hamburguer when click on link */
+navWrapper.addEventListener("click", (e) => {
+  if (e.target.id === "nav") {
+    navWrapper.classList.remove("show");
+    toggleButton.classList.remove("close");
+  }
+});
+
